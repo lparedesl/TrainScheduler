0ee7377af1d21d8687ecdc1a1b1ca81e112be957
luis paredes <luis paredes>
2017-04-04 18:36:45 -0400

    updated js

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -11,21 +11,21 @@ $(document).ready(function($) {
 	firebase.initializeApp(config);
 
 	// FirebaseUI config.
-	var uiConfig = {
-	signInSuccessUrl: "../../index.html",
-	signInOptions: [
-		// Leave the lines as is for the providers you want to offer your users.
-		firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-		// firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-		// firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-		// firebase.auth.GithubAuthProvider.PROVIDER_ID,
-		firebase.auth.EmailAuthProvider.PROVIDER_ID
-	],};
+	// var uiConfig = {
+	// signInSuccessUrl: "../../index.html",
+	// signInOptions: [
+	// 	// Leave the lines as is for the providers you want to offer your users.
+	// 	firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+	// 	// firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+	// 	// firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+	// 	// firebase.auth.GithubAuthProvider.PROVIDER_ID,
+	// 	firebase.auth.EmailAuthProvider.PROVIDER_ID
+	// ],};
 
-	// Initialize the FirebaseUI Widget using Firebase.
-	var ui = new firebaseui.auth.AuthUI(firebase.auth());
-	// The start method will wait until the DOM is loaded.
-	ui.start("#firebaseui-auth-container", uiConfig);
+	// // Initialize the FirebaseUI Widget using Firebase.
+	// var ui = new firebaseui.auth.AuthUI(firebase.auth());
+	// // The start method will wait until the DOM is loaded.
+	// ui.start("#firebaseui-auth-container", uiConfig);
 
 	// initApp = function() {
 	// 	firebase.auth().onAuthStateChanged(function(user) {
